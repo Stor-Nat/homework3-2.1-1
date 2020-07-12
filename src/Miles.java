e5976e28c14b8dfd44dbc43ddf50f6791e939bd7
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-07-12 22:00:51 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        int price = 500060;
+        int miles = price / 20 / 100;
+        System.out.println("Сумма начисленных мили = " + miles);
+    }
+}
